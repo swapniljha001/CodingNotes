725fe25b3dbc9134cc8ebf91a54036f7237bb41a
Swapnil Jha <swapniljha001@gmail.com>
2024-10-01 21:03:10 +0200

    Update GINI_v1.3.py
    Add LinkedIn

--- GINI/GINI_v1.3.py
+++ GINI/GINI_v1.3.py
@@ -238,4 +238,6 @@ if st.button("Submit Further Query"):
     further_finished_chain = chain.run(decision + further_query2 + input_guard + nutrient_feature)
     st.markdown(further_finished_chain, unsafe_allow_html=True)
 
-st.write("<p style='text-align: center;'><i>Made by Swapnil, Shahrukh, and Aakash!</i></p>", unsafe_allow_html=True)
+# st.write("<p style='text-align: center;'><i>Made by Swapnil, Shahrukh, and Akash!</i></p>", unsafe_allow_html=True)
+st.write("<p style='text-align: center;'><i>Made by <a href='https://www.linkedin.com/in/swapniljha001/'>Swapnil Jha</a>, Shahrukh, and Akash!</i></p>", unsafe_allow_html=True)
+
